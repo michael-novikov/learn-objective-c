965a30db9c0919a913cf4706b98825d30db10563
Mikhail Novikov <michael.a.novikov@yandex.ru>
2018-10-20 15:43:13 +0300

    add KaczmarekBook Chapter 3 project

---
+++ KaczmarekBook/Chapter 3/Chapter 3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Chapter 3
+//
+//  Created by  Mikhail Novikov on 20/10/2018.
+//  Copyright © 2018  Mikhail Novikov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
