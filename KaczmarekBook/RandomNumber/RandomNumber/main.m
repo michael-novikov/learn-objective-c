c13e20923f96d75e86b2e57d0fa08aae823356cd
Mikhail Novikov <michael.a.novikov@yandex.ru>
2018-10-20 22:14:58 +0300

    (KaczmarekBook) implement RandomNumber project

--- KaczmarekBook/RandomNumber/RandomNumber/main.m
+++ KaczmarekBook/RandomNumber/RandomNumber/main.m
@@ -11,7 +11,38 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        int randomNumber = 1;
+        int userGuess = 1;
+        BOOL continueGuessing = YES;
+        BOOL keepPlaying = YES;
+        char yesNo = ' ';
+        
+        while (keepPlaying) {
+            randomNumber = (arc4random() % 101);
+            NSLog(@"The random number to guess is: %d", randomNumber);
+            while (continueGuessing) {
+                NSLog(@"Pick a number between 0 and 100. ");
+                scanf("%d", &userGuess);
+                fgetc(stdin); // remove CR/LF i.e extra character
+                if (userGuess == randomNumber) {
+                    continueGuessing = NO;
+                    NSLog(@"Correct number!");
+                }
+                else if (userGuess > randomNumber) {
+                    NSLog(@"Your guess is too high");
+                }
+                else {
+                    NSLog(@"Your guess is too low");
+                }
+            }
+            NSLog(@"Play Again? Y or N");
+            yesNo = fgetc(stdin);
+            if (yesNo == 'N' || yesNo == 'n') {
+                keepPlaying = FALSE;
+            }
+            
+            continueGuessing = TRUE;
+        }
     }
     return 0;
 }
